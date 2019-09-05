115c8e2d402ea654447c073f9fa7c26090373286
Harvey <jhalarde@gmail.com>
2019-09-05 22:08:30 +0800

    Add comments to config file

--- config/larapress.php
+++ config/larapress.php
@@ -1,13 +1,37 @@
 <?php
 
 return [
+	/*
+	 *  The default route call
+	 *  eg. /larapress/123
+	 */
 	'route' => 'larapress',
 
+	/*
+	 * The default title of the Page
+	 * This can be modified in the larapress view
+	 */
 	'page_title' => 'Larapress',
 
+	/*
+	 * The post uri where we get the article post
+	 * eg. http://website.com/json/file/123
+	 */
 	'get_post_uri' => null,
 
+	/*
+	 * The identifier of the response we get from the post uri
+	 * eg. {
+	 *       "response": {
+	 *          "content": "<article>This is the content</article>"
+	 *       }
+	 *    }
+	 *  The identifier should be 'response.content'
+	 */
 	'response_identifier' => null,
 
+	/*
+	 * Flag if the cache should be enabled.
+	 */
 	'cache' => false,
 ];
